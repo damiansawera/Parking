88d51fb2aea2913536a19bd6bb597c92cb7b63dd
Damian Sawera <--unset>
2024-05-21 14:09:46 +0200

    model selection with enums

---
+++ parking-frontend/src/app/enums/ford-models.ts
@@ -0,0 +1,8 @@
+export enum FordModels {
+    Focus = 'Focus',
+    Mustang = 'Mustang',
+    Bronco = 'Bronco',
+    Kuga = 'Kuga',
+    Puma = 'Puma',
+    Ranger = 'Ranger'
+}
---
+++ parking-frontend/src/app/enums/toyota-models.ts
@@ -0,0 +1,6 @@
+export enum ToyotaModels {
+    Corolla = 'Corolla',
+    Camry = 'Camry',
+    RAV4 = 'RAV4',
+    Highlander = 'Highlander',
+}
